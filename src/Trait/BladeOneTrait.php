0f770e811c42ce867dc8eafad24a37d29e195b4d
mjamilasfihani <mohammadjamilasfihani@gmail.com>
2021-08-19 09:33:05 +0700

    add tag

--- src/Trait/BladeOneTrait.php
+++ src/Trait/BladeOneTrait.php
@@ -4,5 +4,27 @@ namespace CI4\FrontEnd\Trait;
 
 trait BladeOneTrait
 {
-	// extending the BladeOne
+	// @csrf_token()
+	protected function compileCsrf_token()
+	{
+		return "<?php echo csrf_token() ?>";
+	}
+
+	// @csrf_header()
+	protected function compileCsrf_header()
+	{
+		return "<?php echo csrf_header() ?>";
+	}
+
+	// @csrf_field()
+	protected function compileCsrf_field()
+	{
+		return "<?php echo csrf_field() ?>";
+	}
+
+	// @csrf_meta()
+	protected function compileCsrf_meta()
+	{
+		return "<?php echo csrf_meta() ?>";
+	}
 }
