4f8fbe09b8697954bb7d41326c2faeb247260b9e
mjamilasfihani <mohammadjamilasfihani@gmail.com>
2021-08-19 09:07:13 +0700

    alpha release ^_^

--- src/Engine.php
+++ src/Engine.php
@@ -21,23 +21,15 @@ class Engine extends Machine
 	protected $data = [];
 
 	/**
-	 * Define the theme name
-	 * 
-	 * @var string $theme
-	 */
-	protected $theme = 'default';
-
-	/**
 	 * Constructor
 	 * 
 	 * @var string $name
 	 * @var array  $data
 	 */
-	protected function __construct(string $name, array $data = [], string $theme = 'default')
+	protected function __construct(string $name, array $data = [])
 	{
-		$this->name  = $name;
-		$this->data  = $data;
-		$this->theme = $theme;
+		$this->name = $name;
+		$this->data = $data;
 	}
 
 	//--------------------------------------------------------------------
@@ -64,16 +56,7 @@ class Engine extends Machine
 	 */
 	public function run()
 	{
-		return $this->journey($this->name, $this->data)->destiny($this->theme);
-	}
-
-	/**
-	 * Themes initialize - OPTIONAL
-	 * 
-	 * @var string $theme
-	 */
-	public function theme(string $alias)
-	{
-		return new self($this->name, $this->data, $alias);
+		return $this->display($this->name, $this->data)
+					->now();
 	}
 }
